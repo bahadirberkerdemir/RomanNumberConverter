90766adef6ebe89831db10a78bc5f6481bfd0905
Bahadır Berk Erdemir <bahadirberkerdemir@gmail.com>
2026-08-20 19:35:46 +0300

    Interface created.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import tkinter as tk
+
+
 class UndefinedNumeralError(Exception):
     pass
 
 class RomanNumeralSyntaxError(Exception):
     pass
 
-def one_digit(str):
-    match str:
+def base_numeral_conversions(roman_numeral):
+    match roman_numeral:
         case 'I':
             return 1
         case 'V':
@@ -23,34 +26,49 @@ def one_digit(str):
         case _:
             raise UndefinedNumeralError("Please enter a roman numeral. (I V X L C D M) ")
 
-"""
-one_digit_roman= input("Enter a roman numeral: ")
-print(one_digit(one_digit_roman))
-"""
 
-def sum_sub_numerals(str):
+def roman_to_arabic_numeral():
     number = 0
     red_flag= False
     wrong_list = ["I"*4, "V"*4, "X"*4, "L"*4, "C"*4, "D"*4]
+    roman_numeral= romanNumeralInput.get().upper()
 
     for element in wrong_list:
-        if element in str:
+        if element in roman_numeral:
             red_flag = True
 
     if red_flag:
         raise RomanNumeralSyntaxError("A roman numeral cannot have 4 consecutive same numerals except 'M'.")
     else:
-        for i in range(len(str)):
-            if max(range(len(str))) >= i+1:
-                if one_digit(str[i]) >= one_digit(str[i+1]):
-                    number += one_digit(str[i])
+        for i in range(len(roman_numeral)):
+            if max(range(len(roman_numeral))) >= i+1:
+                if base_numeral_conversions(roman_numeral[i]) >= base_numeral_conversions(roman_numeral[i + 1]):
+                    number += base_numeral_conversions(roman_numeral[i])
                 else:
-                    number -= one_digit(str[i])
+                    number -= base_numeral_conversions(roman_numeral[i])
             else:
-                number += one_digit(str[i])
+                number += base_numeral_conversions(roman_numeral[i])
 
-        return number
+        outputNumberLabel.config(text=str(number))
+
+#### TKINTER INTERFACE ####
+
+window = tk.Tk()
+window.title("Roman to Arabic Numeral Converter")
+window.minsize(300, 500)
+window.config(background="light green")
 
 
-two_digit_roman= input("Enter a roman numeral: ")
-print(sum_sub_numerals(two_digit_roman))
+inputTextLabel = tk.Label(window, text="Enter A Roman Numeral",bg="light green",fg="black", font="bold",pady=10)
+inputTextLabel.pack()
+
+romanNumeralInput = tk.Entry(width=20)
+romanNumeralInput.pack()
+
+submitButton = tk.Button(text="Convert", height=1, command=roman_to_arabic_numeral)
+submitButton.pack()
+
+outputNumberLabel = tk.Label(window, text="",bg="light green",fg="black", font="bold", pady=10)
+outputNumberLabel.pack()
+
+window.mainloop()
